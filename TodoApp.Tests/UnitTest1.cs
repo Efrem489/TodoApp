69f84f2754ed4afa32c86624551de73e09737c23
Efrem <efremeinikov90@gmail.com>
2025-06-04 16:09:29 +0300

    Add TodoApp.Tests project with NUnit tests for TaskModel

---
+++ TodoApp.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TodoApp.Domain.Entities; 
+
+
+namespace TodoApp.Tests;
+
+
+[TestFixture]
+public class TaskModelTests
+{
+    [Test]
+    public void TaskModel_TitleCannotBeEmpty()
+    {
+        // Arrange
+        var task = new TodoItem { Title = "" };
+        var context = new ValidationContext(task);
+        var results = new List<ValidationResult>();
+
+        // Act
+        var isValid = Validator.TryValidateObject(task, context, results, true);
+
+        // Assert
+        Assert.That(isValid, Is.False);
+        Assert.That(results[0].ErrorMessage, Is.EqualTo("Task title is required"));
+    }
+
+    [Test]
+    public void TaskModel_ValidTitle_PassesValidation()
+    {
+        // Arrange
+        var task = new TodoItem { Title = "Test Task" };
+        var context = new ValidationContext(task);
+        var results = new List<ValidationResult>();
+
+        // Act
+        var isValid = Validator.TryValidateObject(task, context, results, true);
+
+        // Assert
+        Assert.That(isValid, Is.True);
+        Assert.That(results, Is.Empty);
+    }
+}
